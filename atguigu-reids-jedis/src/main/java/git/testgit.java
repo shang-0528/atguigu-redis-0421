ed88d6db4597358203024a12f3fa2870edf20645
shang <shang@163.com>
2020-09-01 20:07:46 +0800

    测试冲突：edit by hotfix

--- atguigu-reids-jedis/src/main/java/git/testgit.java
+++ atguigu-reids-jedis/src/main/java/git/testgit.java
@@ -3,7 +3,7 @@ package git;
 public class testgit {
     public static void main(String[] args) {
     System.out.println("hello~~");
-    System.out.println("hello~~");
+    System.out.println("hello~~");//  edit by hotfix
     System.out.println("hello~~");
     System.out.println("hello~~");
     System.out.println("hello~~");

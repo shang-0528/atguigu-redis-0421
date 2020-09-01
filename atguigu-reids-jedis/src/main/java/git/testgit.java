7b153abfb7bb8ce57bff6f988b0bf83d6b201e6d
shang <shang@163.com>
2020-09-01 20:06:57 +0800

    测试冲突：edit by master

--- atguigu-reids-jedis/src/main/java/git/testgit.java
+++ atguigu-reids-jedis/src/main/java/git/testgit.java
@@ -3,7 +3,7 @@ package git;
 public class testgit {
     public static void main(String[] args) {
     System.out.println("hello~~");
-    System.out.println("hello~~");
+    System.out.println("hello~~");// edit by master
     System.out.println("hello~~");
     System.out.println("hello~~");
     System.out.println("hello~~");

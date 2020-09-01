abd6266eac9ff32558ffea8bdd69eb482caf8d55
shang <shang@163.com>
2020-09-01 20:35:24 +0800

    测试推送：edit by hotfix

--- atguigu-reids-jedis/src/main/java/git/testgit.java
+++ atguigu-reids-jedis/src/main/java/git/testgit.java
@@ -7,8 +7,8 @@ public class testgit {
     System.out.println("hello~~");// edit by master
     System.out.println("hello~~");// edit by resolve confict
     System.out.println("hello~~");
-    System.out.println("hello~~");
-    System.out.println("hello~~");
+    System.out.println("hello~~");// test push
+    System.out.println("hello~~");// // test push1111111
     System.out.println("hello~~");
     System.out.println("hello~~");
 

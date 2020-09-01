b9e611e74ede03a9c05f06eb56d5e38fa2ede376
shang <shang@163.com>
2020-09-01 21:08:01 +0800

    分支实战1 edit by feature-X1

--- atguigu-reids-jedis/src/main/java/git/testgit.java
+++ atguigu-reids-jedis/src/main/java/git/testgit.java
@@ -9,7 +9,7 @@ public class testgit {
     System.out.println("hello~~");
     System.out.println("hello~~");// test push
     System.out.println("hello~~");// // test push1111111
-    System.out.println("hello~~");// edit by feature-X
+    System.out.println("hello~~");// edit by feature-X1
     System.out.println("hello~~");
 
     }

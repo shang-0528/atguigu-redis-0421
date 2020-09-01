0f612a014bf3f0b544561ebceb03ad45ff20dc3d
shang <shang@163.com>
2020-09-01 19:48:41 +0800

    创建了一个类：testGIT

---
+++ atguigu-reids-jedis/src/main/java/git/testgit.java
@@ -0,0 +1,15 @@
+package git;
+
+public class testgit {
+    public static void main(String[] args) {
+    System.out.println("hello~~");
+    System.out.println("hello~~");
+    System.out.println("hello~~");
+    System.out.println("hello~~");
+    System.out.println("hello~~");
+    System.out.println("hello~~");
+    System.out.println("hello~~");
+    System.out.println("hello~~");
+
+    }
+}
